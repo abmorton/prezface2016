49b1091f8beb973f7249d2c7943f3fa1f97bd775
Adam <abmorton@gmail.com>
2015-11-06 23:06:33 -0800

    changed db field model from 'twitter_site' to 'twitter_name'

--- db_helper.py
+++ db_helper.py
@@ -3,30 +3,30 @@ from models import Candidate
 
 # Create the db if needed.
 
-# db.create_all()
+db.create_all()
 
 # This is a helper script to populate our databases with info
  # about the candidates.
 
-hillaryclinton = Candidate('Hillary Clinton', 'New York', 'Democratic Party', 'http://hillaryclinton.com', 'http://www.facebook.com/hillaryclinton', 'http://twitter.com/hillaryclinton')
-martinomalley = Candidate("Martin O'Malley", "Maryland", "Democratic Party", "http://www.martinomalley.com/", "http://www.facebook.com/MartinOMalley", "http://twitter.com/GovernorOMalley")
-berniesanders = Candidate("Bernie Sanders", "Vermont", "Democratic Party", "http://www.berniesanders.com/", "http://www.facebook.com/berniesanders", "http://twitter.com/SenSanders")
+hillaryclinton = Candidate('Hillary Clinton', 'New York', 'Democratic Party', 'http://hillaryclinton.com', 'http://www.facebook.com/hillaryclinton', 'HillaryClinton')
+martinomalley = Candidate("Martin O'Malley", "Maryland", "Democratic Party", "http://www.martinomalley.com/", "http://www.facebook.com/MartinOMalley", "GovernorOMalley")
+berniesanders = Candidate("Bernie Sanders", "Vermont", "Democratic Party", "http://www.berniesanders.com/", "http://www.facebook.com/berniesanders", "SenSanders")
 
-jebbush = Candidate("Jeb Bush", "Florida", "Republican Party", "http://jeb2016.com/", "http://www.facebook.com/jebbush", "http://twitter.com/JebBush")
-bencarson = Candidate("Ben Carson", "Florida", "Republican Party", "http://bencarson.com/", "http://www.facebook.com/DrBenjaminCarson", "http://twitter.com/RealBenCarson")
-chrischristie = Candidate("Chris Christie", "New Jersey", "Republican Party", "http://ChrisChristie.com", "http://www.facebook.com/GovChrisChristie", "http://twitter.com/GovChristie")
-tedcruz = Candidate("Ted Cruz", "Texas", "Republican Party", "http://TedCruz.org", "http://www.facebook.com/TedCruzPage", "http://twitter.com/TedCruz")
-carlyfiorina = Candidate("Carly Fiorina", "Virginia", "Republican Party", "http://CarlyForPresident.com", "http://www.facebook.com/CarlyFiorina", "http://twitter.com/CarlyFiorina")
-jimgilmore = Candidate("Jim Gilmore", "Virginia", "Republican Party", "http://GilmoreForAmerica.com", "http://www.facebook.com/JimGilmore", "http://twitter.com/GovernorGilmore")
-lindseygraham = Candidate("Lindsey Graham", "South Carolina", "Republican Party", "http://LindseyGraham.com", "http://www.facebook.com/LindseGrahamSC", "http://twitter.com/LindseyGrahamSC")
-mikehuckabee = Candidate("Mike Huckabee", "Florida", "Republican Party", "http://MikeHuckabee.com", "http://www.facebook.com/MikeHuckabee", "http://twitter.com/GovMikeHuckabee")
-bobbyjindal = Candidate("Bobby Jindal", "Louisiana", "Republican Party", "http://BobbyJindal.com", "http://www.facebook.com/BobbyJindal", "http://twitter.com/BobbyJindal")
-johnkasich = Candidate("John Kasich", "Ohio", "Republican Party", "http://JohnKasich.com", "http://www.facebook.com/JohnRKasich", "http://twitter.com/JohnRKasich")
-georgepataki = Candidate("George Pataki", "New York", "Republican Party", "http://GeorgePataki.com", "http://www.facebook.com/GovGeorgePataki", "http://twitter.com/GovernorPataki")
-randpaul = Candidate("Rand Paul", "Kentucky", "Republican Party", "http://RandPaul.com", "http://www.facebook.com/RandPaul", "http://twitter.com/RandPaul")
-marcorubio = Candidate("Marco Rubio", "Florida", "Republican Party", "http://MarcoRubio.com", "http://www.facebook.com/MarcoRubio", "http://twitter.com/MarcoRubio")
-ricksantorum = Candidate("Rick Santorum", "Pennsylvania", "Republican Party", "http://RickSantorum.com", "http://www.facebook.com/RickSantorum", "http://twitter.com/RickSantorum")
-donaldtrump = Candidate("Donald Trump", "New York", "Republican Party", "http://DonaldJTrump.com", "http://www.facebook.com/DonaldTrump", "http://twitter.com/RealDonaldTrump")
+jebbush = Candidate("Jeb Bush", "Florida", "Republican Party", "http://jeb2016.com/", "http://www.facebook.com/jebbush", "JebBush")
+bencarson = Candidate("Ben Carson", "Florida", "Republican Party", "http://bencarson.com/", "http://www.facebook.com/DrBenjaminCarson", "RealBenCarson")
+chrischristie = Candidate("Chris Christie", "New Jersey", "Republican Party", "http://ChrisChristie.com", "http://www.facebook.com/GovChrisChristie", "GovChristie")
+tedcruz = Candidate("Ted Cruz", "Texas", "Republican Party", "http://TedCruz.org", "http://www.facebook.com/TedCruzPage", "TedCruz")
+carlyfiorina = Candidate("Carly Fiorina", "Virginia", "Republican Party", "http://CarlyForPresident.com", "http://www.facebook.com/CarlyFiorina", "CarlyFiorina")
+jimgilmore = Candidate("Jim Gilmore", "Virginia", "Republican Party", "http://GilmoreForAmerica.com", "http://www.facebook.com/JimGilmore", "GovernorGilmore")
+lindseygraham = Candidate("Lindsey Graham", "South Carolina", "Republican Party", "http://LindseyGraham.com", "http://www.facebook.com/LindseGrahamSC", "LindseyGrahamSC")
+mikehuckabee = Candidate("Mike Huckabee", "Florida", "Republican Party", "http://MikeHuckabee.com", "http://www.facebook.com/MikeHuckabee", "GovMikeHuckabee")
+bobbyjindal = Candidate("Bobby Jindal", "Louisiana", "Republican Party", "http://BobbyJindal.com", "http://www.facebook.com/BobbyJindal", "BobbyJindal")
+johnkasich = Candidate("John Kasich", "Ohio", "Republican Party", "http://JohnKasich.com", "http://www.facebook.com/JohnRKasich", "JohnRKasich")
+georgepataki = Candidate("George Pataki", "New York", "Republican Party", "http://GeorgePataki.com", "http://www.facebook.com/GovGeorgePataki", "GovernorPataki")
+randpaul = Candidate("Rand Paul", "Kentucky", "Republican Party", "http://RandPaul.com", "http://www.facebook.com/RandPaul", "RandPaul")
+marcorubio = Candidate("Marco Rubio", "Florida", "Republican Party", "http://MarcoRubio.com", "http://www.facebook.com/MarcoRubio", "MarcoRubio")
+ricksantorum = Candidate("Rick Santorum", "Pennsylvania", "Republican Party", "http://RickSantorum.com", "http://www.facebook.com/RickSantorum", "RickSantorum")
+donaldtrump = Candidate("Donald Trump", "New York", "Republican Party", "http://DonaldJTrump.com", "http://www.facebook.com/DonaldTrump", "RealDonaldTrump")
 
 # Add them into the db session
 db.session.add(hillaryclinton)
--- models.py
+++ models.py
@@ -10,17 +10,17 @@ class Candidate(db.Model):
 	campaign_site = db.Column(db.String, unique=True)
 	government_site = db.Column(db.String, unique=True)
 	facebook_site = db.Column(db.String, unique=True)
-	twitter_site = db.Column(db.String, unique=True)
+	twitter_name = db.Column(db.String, unique=True)
 	# photo = 
 	# word_cloud = 
 
-	def __init__(self, name, state, party, campaign_site, facebook_site, twitter_site):
+	def __init__(self, name, state, party, campaign_site, facebook_site, twitter_name):
 		self.name = name
 		self.state = state
 		self.party = party
 		self.campaign_site = campaign_site
 		self.facebook_site = facebook_site
-		self.twitter_site = twitter_site
+		self.twitter_name = twitter_name
 
 	def __repr__(self):
 		return 'id: {}, name: {}, state: {}, party: {}'.format(self.id, self.name, self.state, self.party)
